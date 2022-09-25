e37163b9fb700a24f04a2fb0ec29895a373298cd
Eik Emil Bengtsson <me@eikc.dk>
2022-09-25 13:34:03 +0200

    refactor(parser): refactored parser tests into macro + insta snapshots (#3)

--- src/parser.rs
+++ src/parser.rs
@@ -310,198 +310,95 @@ enum Precedence {
 
 #[cfg(test)]
 mod tests {
-    use crate::ast::{InfixOperator, PrefixOperator};
-
     use super::*;
 
-    #[test]
-    fn parsing_let_statement() {
-        let input = r#"
+    macro_rules! snapshot {
+        ($name:tt, $input:tt) => {
+            #[test]
+            fn $name() {
+                let lexer = Lexer::new($input);
+                let mut parser = Parser::new(lexer);
+
+                let program = parser.parse_program();
+                if parser.errors.len() > 0 {
+                    panic!("error occured in parser: {:#?}", parser.errors);
+                }
+
+                insta::assert_debug_snapshot!(&program);
+            }
+        };
+    }
+
+    snapshot!(parsing_let_statemt, r#"
         let x = 5;
         let y = 10;
         let foo = 838383;
         let x = 1 * 2 * 3 * 4 * 5;
-"#;
-        let expected_statements = vec![
-            Statement::Let("x".to_string(), Expression::Integer(5)),
-            Statement::Let("y".to_string(), Expression::Integer(10)),
-            Statement::Let("foo".to_string(), Expression::Integer(838383)),
-            Statement::Let(
-                "x".to_string(),
-                Expression::Infix(
-                    Box::new(Expression::Infix(
-                        Box::new(Expression::Infix(
-                            Box::new(Expression::Infix(
-                                Box::new(Expression::Integer(1)),
-                                InfixOperator::Multiply,
-                                Box::new(Expression::Integer(2)),
-                            )),
-                            InfixOperator::Multiply,
-                            Box::new(Expression::Integer(3)),
-                        )),
-                        InfixOperator::Multiply,
-                        Box::new(Expression::Integer(4)),
-                    )),
-                    InfixOperator::Multiply,
-                    Box::new(Expression::Integer(5)),
-                ),
-            ),
-        ];
+    "#);
 
-        validate_and_parse_program(input, expected_statements);
-    }
-
-    #[test]
-    fn parsing_return_statement() {
-        let input = r#"
+    snapshot!(parsing_return_statement, r#"
             return 5;
             return 10;
             return add(15);
-        "#;
+    "#);
 
-        let expected_statements = vec![
-            Statement::Return(Expression::Integer(5)),
-            Statement::Return(Expression::Integer(10)),
-            Statement::Return(Expression::Call(
-                Box::new(Expression::Identifier("add".to_string())),
-                vec![Expression::Integer(15)],
-            )),
-        ];
-
-        validate_and_parse_program(input, expected_statements);
-    }
-
-    #[test]
-    fn parsing_identifier_expression() {
-        let input = r#"
+    snapshot!(parsing_identifier_expression, r#"
           foobar;
-        "#;
+    "#);
 
-        let expected_statements = vec![Statement::Expression(Expression::Identifier(
-            "foobar".to_string(),
-        ))];
+    snapshot!(parsing_integer_expression, "5;");
 
-        validate_and_parse_program(input, expected_statements);
-    }
+    snapshot!(parsing_prefix_expression, r#"
+        !5;
+        -15;
+        !true;
+        !false;
+    "#);
 
-    #[test]
-    fn parsing_integer_expression() {
-        let input = r#"
-            5;
-        "#;
+    snapshot!(parsing_infix_expression, r#"
+        5 + 5;
+        5 - 5;
+        5 * 5;
+        5 / 5;
+        5 > 5;
+        5 < 5;
+        5 == 5;
+        5 != 5;
+        true == true;
+        true != false;
+        false == false;
+    "#);
 
-        let expected_statements = vec![Statement::Expression(Expression::Integer(5))];
+    snapshot!(test_parsing_boolean_expression, r#"
+        true;
+        false;
+    "#);
 
-        validate_and_parse_program(input, expected_statements);
-    }
+    snapshot!(test_if_expression, r#"
+        if (x < y) { x }
+    "#);
 
-    #[test]
-    fn parsing_prefix_expression() {
-        let input = r#"
-            !5;
-            -15;
-            !true;
-            !false;
-        "#;
+    snapshot!(test_if_else_expression, r#"
+        if (x < y) { x } else { y }
+    "#);
 
-        let expected_statements = vec![
-            Statement::Expression(Expression::Prefix(
-                PrefixOperator::Bang,
-                Box::new(Expression::Integer(5)),
-            )),
-            Statement::Expression(Expression::Prefix(
-                PrefixOperator::Minus,
-                Box::new(Expression::Integer(15)),
-            )),
-            Statement::Expression(Expression::Prefix(
-                PrefixOperator::Bang,
-                Box::new(Expression::Bool(true)),
-            )),
-            Statement::Expression(Expression::Prefix(
-                PrefixOperator::Bang,
-                Box::new(Expression::Bool(false)),
-            )),
-        ];
+    snapshot!(test_function_parsing, r#"
+        let add = fn(x,y) {
+          x + y
+        }
+    "#);
 
-        validate_and_parse_program(input, expected_statements);
-    }
+    snapshot!(test_function_paramenter_parsing, r#"
+        fn() {};
+        fn(x) {};
+        fn(x, y, z) {};
+    "#);
 
-    #[test]
-    fn parsing_infix_expression() {
-        let input = r#"
-            5 + 5;
-            5 - 5;
-            5 * 5;
-            5 / 5;
-            5 > 5;
-            5 < 5;
-            5 == 5;
-            5 != 5;
-            true == true;
-            true != false;
-            false == false;
-        "#;
+    snapshot!(test_call_expressions, r#"
+        add(1, 2 * 3, 4 + 5);
+    "#);
 
-        let expected_statements = vec![
-            new_infix_expression(
-                Expression::Integer(5),
-                InfixOperator::Plus,
-                Expression::Integer(5),
-            ),
-            new_infix_expression(
-                Expression::Integer(5),
-                InfixOperator::Minus,
-                Expression::Integer(5),
-            ),
-            new_infix_expression(
-                Expression::Integer(5),
-                InfixOperator::Multiply,
-                Expression::Integer(5),
-            ),
-            new_infix_expression(
-                Expression::Integer(5),
-                InfixOperator::Divide,
-                Expression::Integer(5),
-            ),
-            new_infix_expression(
-                Expression::Integer(5),
-                InfixOperator::GT,
-                Expression::Integer(5),
-            ),
-            new_infix_expression(
-                Expression::Integer(5),
-                InfixOperator::LT,
-                Expression::Integer(5),
-            ),
-            new_infix_expression(
-                Expression::Integer(5),
-                InfixOperator::Equals,
-                Expression::Integer(5),
-            ),
-            new_infix_expression(
-                Expression::Integer(5),
-                InfixOperator::NotEquals,
-                Expression::Integer(5),
-            ),
-            new_infix_expression(
-                Expression::Bool(true),
-                InfixOperator::Equals,
-                Expression::Bool(true),
-            ),
-            new_infix_expression(
-                Expression::Bool(true),
-                InfixOperator::NotEquals,
-                Expression::Bool(false),
-            ),
-            new_infix_expression(
-                Expression::Bool(false),
-                InfixOperator::Equals,
-                Expression::Bool(false),
-            ),
-        ];
-
-        validate_and_parse_program(input, expected_statements);
-    }
+    snapshot!(test_call_parameter_parsing, r#""#);
 
     #[test]
     fn test_operator_precedence_parsing() {
@@ -541,165 +438,4 @@ mod tests {
         }
     }
 
-    #[test]
-    fn test_parsing_boolean_expression() {
-        let input = r#"
-            true;
-            false;
-        "#;
-
-        let expected_statements = vec![
-            Statement::Expression(Expression::Bool(true)),
-            Statement::Expression(Expression::Bool(false)),
-        ];
-
-        validate_and_parse_program(input, expected_statements);
-    }
-
-    #[test]
-    fn test_if_expression() {
-        let input = r#"
-            if (x < y) { x }
-        "#;
-
-        let expected_statements = vec![Statement::Expression(Expression::If(
-            Box::new(Expression::Infix(
-                Box::new(Expression::Identifier("x".to_string())),
-                InfixOperator::LT,
-                Box::new(Expression::Identifier("y".to_string())),
-            )),
-            vec![Statement::Expression(Expression::Identifier(
-                "x".to_string(),
-            ))],
-            Vec::new(),
-        ))];
-
-        validate_and_parse_program(input, expected_statements);
-    }
-
-    #[test]
-    fn test_if_else_expression() {
-        let input = r#"
-            if (x < y) { x } else { y }
-        "#;
-
-        let expected_statements = vec![Statement::Expression(Expression::If(
-            Box::new(Expression::Infix(
-                Box::new(Expression::Identifier("x".to_string())),
-                InfixOperator::LT,
-                Box::new(Expression::Identifier("y".to_string())),
-            )),
-            vec![Statement::Expression(Expression::Identifier(
-                "x".to_string(),
-            ))],
-            vec![Statement::Expression(Expression::Identifier(
-                "y".to_string(),
-            ))],
-        ))];
-
-        validate_and_parse_program(input, expected_statements);
-    }
-
-    fn new_infix_expression(
-        left: Expression,
-        operator: InfixOperator,
-        right: Expression,
-    ) -> Statement {
-        Statement::Expression(Expression::Infix(Box::new(left), operator, Box::new(right)))
-    }
-
-    #[test]
-    fn test_function_parsing() {
-        let input = r#"
-            let add = fn(x,y) {
-              x + y
-            }
-        "#;
-
-        let expected_statements = vec![Statement::Let(
-            "add".to_string(),
-            Expression::Function(
-                vec!["x".to_string(), "y".to_string()],
-                vec![Statement::Expression(Expression::Infix(
-                    Box::new(Expression::Identifier("x".to_string())),
-                    InfixOperator::Plus,
-                    Box::new(Expression::Identifier("y".to_string())),
-                ))],
-            ),
-        )];
-
-        validate_and_parse_program(input, expected_statements);
-    }
-
-    #[test]
-    fn test_function_paramenter_parsing() {
-        let input = r#"
-            fn() {};
-            fn(x) {};
-            fn(x, y, z) {};
-        "#;
-
-        let expected_statements = vec![
-            Statement::Expression(Expression::Function(vec![], vec![])),
-            Statement::Expression(Expression::Function(vec!["x".to_string()], vec![])),
-            Statement::Expression(Expression::Function(
-                vec!["x".to_string(), "y".to_string(), "z".to_string()],
-                vec![],
-            )),
-        ];
-
-        validate_and_parse_program(input, expected_statements);
-    }
-
-    #[test]
-    fn test_call_expressions() {
-        let input = r#"
-            add(1, 2 * 3, 4 + 5);
-        "#;
-
-        let expected_statements = vec![Statement::Expression(Expression::Call(
-            Box::new(Expression::Identifier("add".to_string())),
-            vec![
-                Expression::Integer(1),
-                Expression::Infix(
-                    Box::new(Expression::Integer(2)),
-                    InfixOperator::Multiply,
-                    Box::new(Expression::Integer(3)),
-                ),
-                Expression::Infix(
-                    Box::new(Expression::Integer(4)),
-                    InfixOperator::Plus,
-                    Box::new(Expression::Integer(5)),
-                ),
-            ],
-        ))];
-
-        validate_and_parse_program(input, expected_statements);
-    }
-
-    #[test]
-    fn test_call_parameter_parsing() {}
-
-    fn validate_and_parse_program(input: &str, expected_statements: Vec<Statement>) {
-        let lexer = Lexer::new(input);
-        let mut parser = Parser::new(lexer);
-
-        let program = parser.parse_program();
-        if parser.errors.len() > 0 {
-            panic!("error occured in parser: {:#?}", parser.errors);
-        }
-
-        if expected_statements.len() != program.statements.len() {
-            panic!("the program statements and expected statements are not the same. expected: {}, got: {}\n\n {:#?}",
-                expected_statements.len(),
-                program.statements.len(),
-                program.statements);
-        }
-
-        let pairs = expected_statements.iter().zip(program.statements.iter());
-
-        for (expected, actual) in pairs {
-            assert_eq!(expected, actual);
-        }
-    }
 }
